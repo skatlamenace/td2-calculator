277b99ec51f82425b411a8bb09d129469965e35b
skatlamenace <66825424+skatlamenace@users.noreply.github.com>
2020-10-17 17:44:24 +0200

    Add files via upload

---
+++ calculator/src/main/java/calculator/Calculator.java
@@ -0,0 +1,45 @@
+package calculator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+	
+		return a+b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public void longCalculation() {
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Set<Integer> digitsSet(int number) {
+		Set<Integer> integers = new HashSet<Integer>();
+		String numberString = String.valueOf(number);
+
+		for (int i = 0; i < numberString.length(); i++) {
+			if (numberString.charAt(i) != '-') {
+				integers.add(Integer.parseInt(numberString, i, i + 1, 10));
+			}
+		}
+		return integers;
+	}
+	
+	public double add(double a, double b) {
+		return a + b;
+	}
+
+	public double multiply(double a, double b) {
+		return a * b;
+	}
+
+}
